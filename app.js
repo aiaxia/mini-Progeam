7ffbbc8026257c5d27562e6397ef8944e859143d
张占永 <zzy0523@gmail.com>
2018-08-06 18:28:03 +0800

    Merge branch 'master' of https://github.com/aiaxia/mini-Progeam
    # Conflicts:
    #	app.js

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ App({
     brandId: undefined,
     brandName: '',
     seriesId: undefined,
-    seriesId: undefined,
     seriesName: '',
     levelId: undefined,
     timeModel: '',
     timeModelName: '',
+      userPhone:''
   },
   onLaunch: function () {
     // 展示本地存储能力
@@ -20,6 +20,13 @@ App({
     wx.login({
       success: res => {
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
+          //发起网络请求
+          if(res.code){
+              console.log(res.code)
+              // wx.getPhoneNumber(res)
+          }else{
+              console.log('获取用户登录失败！'+res.errMsg)
+          }
       }
     })
     // 获取用户信息
@@ -43,7 +50,7 @@ App({
       }
     })
   },
-  globalData: {
+    globalData: {
     userInfo: null
   }
 })
